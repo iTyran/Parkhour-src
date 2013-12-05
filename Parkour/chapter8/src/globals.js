a256510f955eea6edbc1246c43559f68ea0a2f3b
u0u0 <lkncastle@gmail.com>
2013-12-05 14:18:42 +0800

    chapter 8, object layer of tiled map.

---
+++ Parkour/chapter8/src/globals.js
@@ -0,0 +1,18 @@
+g_groundHight = 63;
+g_runnerStartX = 80;
+
+// collision type for chipmunk
+if(typeof TagOfLayer == "undefined") {
+    var TagOfLayer = {};
+    TagOfLayer.background = 0;
+    TagOfLayer.Animation = 1;
+    TagOfLayer.Status = 2;
+};
+
+// collision type for chipmunk
+if(typeof SpriteTag == "undefined") {
+    var SpriteTag = {};
+    SpriteTag.runner = 0;
+    SpriteTag.coin = 1;
+    SpriteTag.rock = 2;
+};
