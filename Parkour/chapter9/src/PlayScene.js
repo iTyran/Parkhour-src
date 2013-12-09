37bd16ea4249df494d0e0bf6506677fb0f6e9549
u0u0 <lkncastle@gmail.com>
2013-12-09 11:24:30 +0800

    chapter 9

---
+++ Parkour/chapter9/src/PlayScene.js
@@ -0,0 +1,64 @@
+var PlayScene = cc.Scene.extend({
+    space:null,// chipmunk space
+    shapesToRemove:[],
+
+    onEnter:function () {
+        this._super();
+
+        cc.Director.getInstance().resume();
+        this.shapesToRemove = [];
+
+        this.initPhysics();
+        this.addChild(new BackgroundLayer(this.space), 0, TagOfLayer.background);
+        this.addChild(new AnimationLayer(this.space), 0, TagOfLayer.Animation);
+        this.addChild(new StatusLayer(), 0, TagOfLayer.Status);
+
+        //Schedules the "update" method
+        this.scheduleUpdate();
+    },
+
+    // init space of chipmunk
+    initPhysics:function() {
+        this.space = new cp.Space();
+        // Gravity
+        this.space.gravity = cp.v(0, -350);
+        // set up Walls
+        var wallBottom = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, g_groundHight),// start point
+            cp.v(4294967295, g_groundHight),// MAX INT:4294967295
+            0);// thickness of wall
+        this.space.addStaticShape(wallBottom);
+        // setup chipmunk CollisionHandler
+        this.space.addCollisionHandler(SpriteTag.runner, SpriteTag.coin,
+            this.collisionCoinBegin.bind(this), null, null, null);
+        this.space.addCollisionHandler(SpriteTag.runner, SpriteTag.rock,
+            this.collisionRockBegin.bind(this), null, null, null);
+    },
+
+    collisionCoinBegin:function (arbiter, space) {
+        var shapes = arbiter.getShapes();
+        // shapes[0] is runner
+        this.shapesToRemove.push(shapes[1]);
+
+        var statusLayer = this.getChildByTag(TagOfLayer.Status);
+        statusLayer.addCoin(1);
+    },
+
+    collisionRockBegin:function (arbiter, space) {
+        cc.Director.getInstance().pause();
+        this.addChild(new GameOverLayer());
+    },
+
+    update:function (dt) {
+        // chipmunk step
+        this.space.step(dt);
+
+        // Simulation cpSpaceAddPostStepCallback
+        for(var i = 0; i < this.shapesToRemove.length; i++) {
+            var shape = this.shapesToRemove[i];
+            this.getChildByTag(TagOfLayer.background).removeObjectByShape(shape);
+        }
+        this.shapesToRemove = [];
+    }
+});
+
---
+++ Parkour/chapter9/src/resource.js
@@ -0,0 +1,30 @@
+var s_HelloBG = "res/helloBG.png";
+var s_start_n = "res/start_n.png";
+var s_start_s = "res/start_s.png";
+var s_restart_n = "res/restart_n.png";
+var s_restart_s = "res/restart_s.png";
+var s_runner = "res/runner.png";
+var s_runnerplist = "res/runner.plist";
+
+var s_background = "res/background.png";
+var s_backgroundplist = "res/background.plist";
+
+var s_map = "res/map.png";
+var s_map00 = "res/map00.tmx";
+var s_map01 = "res/map01.tmx";
+
+var g_resources = [
+    //image
+    {src:s_HelloBG},
+    {src:s_start_n},
+    {src:s_start_s},
+    {src:s_restart_n},
+    {src:s_restart_s},
+    {src:s_runner},
+    {src:s_runnerplist},
+    {src:s_background},
+    {src:s_backgroundplist},
+    {src:s_map},
+    {src:s_map00},
+    {src:s_map01}
+];
